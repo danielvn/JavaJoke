247afa89751978f63b7556b835a533859fa441d5
Daniel Vega <daniel@10.14.28.76>
2018-04-20 13:47:48 -0500

    Flow 1

--- src/zootopia/Zootopia.java
+++ src/zootopia/Zootopia.java
@@ -22,7 +22,6 @@ public class Zootopia extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("Zootopia.fxml"));
         
         Scene scene = new Scene(root);
-        
         stage.setScene(scene);
         stage.show();
     }
